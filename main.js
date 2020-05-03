a44025dae8f3dbbb430808578258da6fb62669fd
Toshiyuki Masui <masui@pitecan.com>
2020-05-03 19:54:36 +0900

    ...

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ const path = require('path')
 const fs = require('fs');
 const Gyazo  = require('gyazo-api');
 const { app, ipcMain, BrowserWindow } = require('electron')
+const ExifImage = require('exif').ExifImage;
 
 
 // 起動 URL
@@ -26,38 +27,65 @@ app.on('window-all-closed', function() {
 
 let token = ''
 let tokenpath = `${app.getPath('userData')}/gyazotoken`
-console.log(`tokenpath = ${tokenpath}`)
+//console.log(`tokenpath = ${tokenpath}`)
 try {
     fs.statSync(tokenpath)
     token = fs.readFileSync(app.getPath('userData')+"/gyazotoken",token)
 }
 catch (err){
 }
-console.log(`token = ${token}`)
+//console.log(`token = ${token}`)
 
-// 非同期メッセージの受信と返信
+let gyazo = new Gyazo(token);
+
+//
+// ブラウザプロセスから伝えられたアップロードファイルをGyazo.comに登録
+//
 ipcMain.on('asynchronous-message', (event, path, token) => {
     // 受信したコマンドの引数を表示する
-    console.log(`async-message: path=${path}`)
-    console.log(token)
-
+    //console.log(`async-message: path=${path}`)
+    //console.log(`token = ${token}`)
+	   
     fs.writeFileSync(app.getPath('userData')+"/gyazotoken",token)
+    
+    let data = fs.readFileSync(path)
+    
+    
+    
+    try {
+	let t = null
+    	new ExifImage({ image : path }, function (error, exifData) {
+            if (error)
+		console.log('Error: '+error.message);
+            else {
+		//console.log(exifData); // Do something with your data!
+		//console.log(exifData.image.ModifyDate)
+		//console.log("----------")
+		//let datedesc = new Date(Date.parse(exifData.image.ModifyDate.replace(/:/,'/').replace(/:/,'/')))
+		let t = Date.parse(exifData.image.ModifyDate.replace(/:/,'/').replace(/:/,'/')) / 1000.0
+		//console.log(t)
 
-    let data = fs.createReadStream(path)
-    let gyazo = new Gyazo(token);
-    gyazo.upload(data, {
-	title: "my picture",
-	desc: "upload from nodejs"
-    })
-	.then(function(res){
-	    console.log("Success")
-	    console.log(res.data.image_id);
-	    console.log(res.data.permalink_url);
+		let stream = fs.createReadStream(path)
+		//console.log(stream)
+		gyazo.upload(stream, {
+		    title: "localhost",
+		    desc: exifData.image.ModifyDate,
+		    created_at: t
+		})
+		    .then(function(res){
+			console.log("Success")
+			console.log(res.data.image_id);
+			console.log(res.data.permalink_url);
+		    })
+		    .catch(function(err){
+			console.log("ERROR")
+			console.error(err);
+		    });
+	    }
 	})
-	.catch(function(err){
-	    console.log("ERROR")
-	    console.error(err);
-	});
+    }
+    catch (err){
+    }
 })
 
 
@@ -76,7 +104,7 @@ app.on('ready', function() {
     
     // デベロッパーツールを表示
     // 不要であればコメントアウト
-    //mainWindow.toggleDevTools();
+    // mainWindow.toggleDevTools();
     
     // ウィンドウが閉じられたらアプリも終了
     mainWindow.on('closed', function() {
