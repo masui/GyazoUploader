3b99125ac995b49f7500c7a8acb209a1403d28a1
Toshiyuki Masui <masui@pitecan.com>
2020-05-03 16:56:19 +0900

    ...

--- preload.js
+++ preload.js
@@ -1,21 +1,24 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
-window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
+//
+// IPCで必要になる「ipcRenderer」をここで定義
+//
 
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
-  }
+window.addEventListener('DOMContentLoaded', () => {
+    const replaceText = (selector, text) => {
+	const element = document.getElementById(selector)
+	if (element) element.innerText = text
+    }
+    
+    for (const type of ['chrome', 'node', 'electron']) {
+	replaceText(`${type}-version`, process.versions[type])
+    }
 })
 
 const { ipcRenderer } = require('electron');
 
 process.once('loaded', () => {
-  global.native = {
-    ipcRenderer: ipcRenderer,
-  };
+    global.native = {
+	ipcRenderer: ipcRenderer,
+    };
 });
-
