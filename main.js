5745f85483f2ff3eac62f95d0973cec5009607e2
Toshiyuki Masui <masui@pitecan.com>
2020-05-05 22:39:34 +0900

    緯度経度情報をちゃんと反映

--- main.js
+++ main.js
@@ -47,6 +47,7 @@ ipcMain.on('file', (event, path, token) => {
     let t = Date.parse(stat.mtime)
     let desc =''
     try {
+	let locinfo = ''
     	new ExifImage({ image : path }, function (error, exifData) {
             if(error){
 		event.sender.send('gyazo', `Exif error: ${error.message}`);
@@ -56,23 +57,37 @@ ipcMain.on('file', (event, path, token) => {
 		t = Date.parse(exifData.image.ModifyDate.replace(/:/,'/').replace(/:/,'/'))
 		desc = exifData.image.ModifyDate
 		event.sender.send('gyazo', "Exif found");
+
+		let lat = exifData.gps.GPSLatitude
+		if(lat){
+		    let la = lat[0] + lat[1]/60.0 + lat[2] / 3600.0
+		    if(exifData.gps.GPSLatitudeRef == 'S'){
+			la = -la
+		    }
+		    let long = exifData.gps.GPSLongitude
+		    let lo = long[0] + long[1]/60.0 + long[2] / 3600.0
+		    if(exifData.gps.GPSLongitudeRef == 'W'){
+			lo = -lo
+		    }
+		    locinfo = `https://www.google.com/maps/@${la},${lo},19z`
+		}
 	    }
-	    let stream = fs.createReadStream(path)
-	    gyazo.upload(stream, {
-		title: "localhost", // もっとちゃんとしたものを指定したい
-		desc: desc,
-		created_at: t / 1000.0
-	    })
-		.then(function(res){
-		    //console.log(res.data.image_id);
-		    //console.log(res.data.permalink_url);
-		    event.sender.send('gyazo', `Upload success: ${res.data.permalink_url}`);
-		    event.sender.send('gyazo', `Timestamp: ${new Date(t)}`)
-		})
-		.catch(function(err){
-		    event.sender.send('gyazo', `Upload error: ${error.message}`)
-		});
 	})
+	let stream = fs.createReadStream(path)
+	gyazo.upload(stream, {
+	    title: "localhost", // もっとちゃんとしたものを指定したい
+	    desc: locinfo,
+	    created_at: t / 1000.0
+	})
+	    .then(function(res){
+		//console.log(res.data.image_id);
+		//console.log(res.data.permalink_url);
+		event.sender.send('gyazo', `Upload success: ${res.data.permalink_url}`);
+		event.sender.send('gyazo', `Timestamp: ${new Date(t)}`)
+	    })
+	    .catch(function(err){
+		event.sender.send('gyazo', `Upload error: ${error.message}`)
+	    });
     }
     catch (err){
     }
